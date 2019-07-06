34d19b56d2b13bafaaa40a62fdb5928aa3f610c5
TSC2004421 <atsuya.kouchi@tech-sc.co.jp>
2019-07-06 16:20:32 +0900

    メイクまでちゃんとできるやつ

---
+++ main.c
@@ -0,0 +1,27 @@
+/************************************************************************************//**
+ * @file    main.c
+ * @brief   STM32F303RE STM32Nucleo
+ *
+ * @mainpage STM32Nucleo STM32F303RE 向けプロジェクト
+ * @brief   組込みシステム技術会合用プロジェクトである.<br>
+ *
+ * @details STM32CubeF3ライブラリを使用して実装する.<br>
+ *
+ * @par 変更履歴:
+ *	Rev0.01　2019/06/02　テンプレート作成<br>
+ * @par Copyright
+ *	2014-19 Emb-se.com All rights reserved.
+ */
+#include <stdio.h>
+#include "stm32f3xx.h"
+
+
+/************************************************************************************//**
+ * @brief   プログラムメイン
+ * @param   None
+ * @retval  None
+ */
+void main( void )
+{
+	for(;;);
+}
